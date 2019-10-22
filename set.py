81d442d4f96f26a9e6de867af3e4133cc7aa724c
websiteaap <53229705+websiteaap@users.noreply.github.com>
2019-10-22 12:59:04 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,18 @@
+print ("\t\tAll operation of the set \n")
+a = {1,2,3,4,5,6,78}
+b = {12,1,2,3,4,5,6,7,8,9,8,77,45}
+print (a)
+print (b)
+print ("element pop of in the set  of a")
+a.pop()
+print (a)
+print ("element pop of the set of b ")
+b.pop()
+print (b)
+print ("calculate of the length of set a")
+c = len(a)
+print ("length of set a",c)
+
+print ("calculate of the length of set b")
+c = len(b)
+print ("length of set b",c)
